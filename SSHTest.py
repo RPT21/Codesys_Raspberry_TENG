17f05c189edaae79911655cd8ea98c73492dd061
RPT21 <marremiau@outlook.com>
2025-05-06 19:28:50 +0200

    Second update
    Update SSH Python Script Paramiko.
    
    Now we can record and move LinMot at the same time.

--- SSHTest.py
+++ SSHTest.py
@@ -1,35 +1,57 @@
 import paramiko
+import stat
 
 hostname = "192.168.100.200"
 port = 22
 username = "TENG"
 password = "raspberry"
 
+ruta_remota = "/var/opt/codesys/PlcLogic/FTP_Folder"
+ruta_local = "archivo_descargado.txt"
+
 # Create SSH client
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Automatically add unknown hosts
 
+# Important, els noms de fitxers han d'anar entre "" 
+# ja que les () en la terminal es consideren per fer subprocessos o arrays
+
+def stop_codesys():
+    stdin, stdout, stderr = ssh.exec_command("sudo service codesyscontrol stop")
+    # Esperar a que termine el comando
+    exit_status = stdout.channel.recv_exit_status()
+    print("codesyscontrol stopped")
+    
+def start_codesys():
+    stdin, stdout, stderr = ssh.exec_command("sudo service codesyscontrol start")
+    # Esperar a que termine el comando
+    exit_status = stdout.channel.recv_exit_status()
+    print("codesyscontrol started")
+    
 try:
     # Connect to the host
     ssh.connect(hostname, port=port, username=username, password=password)
+    
+    # Iniciar sesión SFTP
+    sftp = ssh.open_sftp()
+    lista_ficheros = sftp.listdir(ruta_remota)
+    # sftp.get(ruta_remota + "/" + lista_ficheros[0], "datafile.csv")  # Descarregar fitxer al PC
+    
+    # Obtenir nomes fitxers, excloure les carpetes
+    entradas = sftp.listdir_attr(ruta_remota)
+    solo_archivos = [e.filename for e in entradas if stat.S_ISREG(e.st_mode)]
 
     # Run a command
     # stdin, stdout, stderr = ssh.exec_command("ls -l /home/TENG")
     # print("Output:")
     # print(stdout.read().decode())
     
-    stdin, stdout, stderr = ssh.exec_command("sudo service codesyscontrol stop")
-    # Esperar a que termine el comando
-    exit_status = stdout.channel.recv_exit_status()  # Esto bloquea hasta que el comando 
-    print("codesyscontrol stopped")
-    
-    stdin, stdout, stderr = ssh.exec_command("sudo service codesyscontrol start")
-    # Esperar a que termine el comando
-    exit_status = stdout.channel.recv_exit_status()  # Esto bloquea hasta que el comando 
-    print("codesyscontrol started")
+    # stop_codesys()
+    # start_codesys()
 
     # Close the connection
     # ssh.close()
+    # sftp.close()
 except paramiko.AuthenticationException:
     print("Authentication failed.")
 except paramiko.SSHException as e:
